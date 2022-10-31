0525d858381a4a2d5eb62814d8bacb415a2b3ddf
hyzhangs <34256576+hyzhangs@users.noreply.github.com>
2022-10-31 12:16:03 +0800

    2022.10.31 28所联合训练门户 二开应用自定义顶栏搜索部 top custom react plugin (#332)
    * 项目初始化
    
    * fix：修改gitignore文件
    
    * 2022.5.24达远慢病管理 象限图 react version analyzer (#16)
    
    * vue版本分析仪二开初始化
    
    * fix：修改vue版本分析仪模板
    
    * 需求地址：https://smartdata.feishu.cn/docs/doccnYFZ8juwaJLXp2HkeDcTONf#BCEkV1
    
    * 象限表
    
    * 压缩包
    
    * 接口文档 (#5)
    
    * 2022.5.24达远慢病管理 象限图 vue version analyzer (#13)
    
    * 接口文档
    
    * 标题配置
    
    Co-authored-by: Orochi-sx <41367083+Orochi-sx@users.noreply.github.com>
    
    * 配置信息名字过长问题修改
    
    Co-authored-by: jiangsixian <jiangsixian@njsdata.com>
    Co-authored-by: Orochi-sx <41367083+Orochi-sx@users.noreply.github.com>
    
    * Revert "2022.5.24达远慢病管理 象限图 react version analyzer (#16)"
    
    This reverts commit f8b09bb08d214f558b52b76fdcb642b1aad34384.
    
    * feat:新增readme文件
    
    * feat:完善readme文档
    
    * feat:完善readme文档
    
    * fix:修改readme文档错别字
    
    * fix：修改gitignore文件
    
    * feat:完善readme文档
    
    * feat：更新md文档
    
    * feat：更新md文档
    
    * feat：更新md文档
    
    * feat：添加工具类
    
    * fix：修改文档
    
    * fix：修改gitignore文件
    
    * feat:react版本应用头部插件模板初始化
    
    * 完成 (#104)
    
    * feat: 修改自适应宽 (#105)
    
    * feat: 修改自适应宽
    
    * feat: 样式优化
    
    * feat : 接口文档
    
    * 2022.7.13 南山 顶栏自定义扩展 react version app head plugin (#118)
    
    * feat: 修改自适应宽
    
    * feat: 样式优化
    
    * feat : 接口文档
    
    * 1
    
    * feat: '字号样式修改'
    
    * faet: 时间戳转换
    
    * feat: SR-W44-ROwkDK , 二开应用自定义顶栏搜索部分
    
    Co-authored-by: jiangsixian <jiangsixian@njsdata.com>
    Co-authored-by: BianGongCheng <66733481+BianGongCheng@users.noreply.github.com>
    Co-authored-by: Orochi-sx <41367083+Orochi-sx@users.noreply.github.com>
    Co-authored-by: jiangsixian <763653631@qq.com>
    Co-authored-by: YoungRuntu <97159451+YoungRuntu@users.noreply.github.com>
    Co-authored-by: zhanghongyi <zhanghongyi@njsdata.com>

---
+++ dumi/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/zhanghongyi/workspace/二开/web-secondary--development/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
---
+++ dumi/.umi/dumi/demos/index.ts
@@ -0,0 +1,41 @@
+// @ts-nocheck
+import React from 'react';
+import { dynamic } from 'dumi';
+import rawCode1 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/list.jsx?dumi-raw-code';
+import rawCode2 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/src/App.jsx?dumi-raw-code';
+import rawCode3 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/src/components/list/index.js?dumi-raw-code';
+import rawCode4 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/src/components/list/index.less?dumi-raw-code';
+import rawCode5 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/src/components/designConfiguration/index.js?dumi-raw-code';
+import rawCode6 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/mockComponentCenter.ts?dumi-raw-code';
+import rawCode7 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/designConfiguration.jsx?dumi-raw-code';
+import rawCode8 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/formMain.jsx?dumi-raw-code';
+import rawCode9 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/formChild.jsx?dumi-raw-code';
+import rawCode10 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/table.jsx?dumi-raw-code';
+import rawCode11 from '!!dumi-raw-code-loader!/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/detail.jsx?dumi-raw-code';
+
+export default {
+  'web-secondary--development-list': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/list.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode1},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode6}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"},"react-dom":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"}},"identifier":"web-secondary--development-list"},
+  },
+  'web-secondary--development-designconfiguration': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/designConfiguration.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode7},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode6}},"dependencies":{"react":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"}},"identifier":"web-secondary--development-designconfiguration"},
+  },
+  'web-secondary--development-formmain': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/formMain.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode8},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode6}},"dependencies":{"react":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"}},"identifier":"web-secondary--development-formmain"},
+  },
+  'web-secondary--development-formchild': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/formChild.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode9},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode6}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"},"react-dom":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"}},"identifier":"web-secondary--development-formchild"},
+  },
+  'web-secondary--development-table': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/table.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode10},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"},"react-dom":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"}},"identifier":"web-secondary--development-table"},
+  },
+  'web-secondary--development-detail': {
+    component: (require('/Users/zhanghongyi/workspace/二开/web-secondary--development/demo/detail.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode11},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/components/list/index.less":{"import":"./index.less","content":rawCode4},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode5}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"@ant-design/icons":{"version":"4.7.0"},"react-dom":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"}},"identifier":"web-secondary--development-detail"},
+  },
+};
